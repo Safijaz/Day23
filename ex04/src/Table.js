96ede463a1c9ba0ae2078651545638ae1819c720
Safijaz <safija.zahirovic@gmail.com>
2021-03-30 21:48:50 +0200

    commit

---
+++ ex04/src/Table.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+   
+const Table =(props)=> {
+  const  {characterData,removeCharacter} =props;
+  
+  return (
+      <table>
+          <TableHeader />
+          <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+          </table>
+  )
+}
+        <tr key={index}>
+        <td>{row.firstName}</td>
+        <td>{row.lastName}</td>
+        <td>
+            <button onClick={() =>props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
+   
---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// Remove  code below this line
+
+
+// Remove code below this line 
+ReactDOM.render(<App/>, document.getElementById('root'));
